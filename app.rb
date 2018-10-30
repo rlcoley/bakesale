db8717bd81888557bb87aa948d133a77b6f595e8
rlcoley <rosscoley1002@gmail.com>
2018-10-30 17:17:18 -0400

    added add to cart button for each item. Put a onclick function in the button. set up 3 api calls for each function that counts how many times an item is clicked then adds to cart

--- app.rb
+++ app.rb
@@ -9,12 +9,36 @@ end
 
 get "/cakes" do
 
+  class Cake
+    attr_accessor :name, :price, :url, :description
+    def initialize(name,price,description, url)
+      @name = name
+      @price = price
+      @description = description
+      @url = url
+    end
+
+  end
+
+@cake1 = Cake.new('Chocolate cake',6," Can't go wrong with Chocolate Cake", "https://nichaliciousbaking.files.wordpress.com/2015/09/dsc07814.jpg")
+
   erb :cakes
 end
 
 
 get "/pies" do
+  class Pie
+    attr_accessor :name, :price, :url, :description
+    def initialize(name,price,description, url)
+      @name = name
+      @price = price
+      @description = description
+      @url = url
+    end
 
+  end
+
+@pie1 = Pie.new('Apple Pie ',6,"Issa Pie", "http://www.pastrycraftseattle.com/blog/wp-content/uploads/2010/11/Pie.jpg")
    erb :pies
 end
 
@@ -31,9 +55,43 @@ get "/cookies" do
 
   end
 
-@c1 = Cookie.new('Chocolate Chip',6,"Chocolate Chip, its a classic", "https://images-gmi-pmc.edge-generalmills.com/e8198dd2-770b-4c7c-a748-ca7538cf48d0.jpg")
-
-
+@cookie1 = Cookie.new('Chocolate Chip',6,"Chocolate Chip, its a classic", "https://images-gmi-pmc.edge-generalmills.com/e8198dd2-770b-4c7c-a748-ca7538cf48d0.jpg")
 
   erb :cookies
+end
+
+cookieNum = 0
+get '/test' do
+
+# add a cookie cart
+cookieNum += 1
+p cookieNum
+  if cookieNum == 1
+    p "You have  #{cookieNum} cookie total"
+  else
+    p "You have #{cookieNum} cookies total"
+  end
+
+end
+
+# add a cake to cart
+cakeNum = 0
+get '/buycake' do
+  cakeNum += 1
+  p "You have #{cakeNum} cake total"
+end
+
+
+# add a pie to cart
+pieNum = 0
+get '/buypie' do
+  pieNum += 1
+  p "You have #{pieNum} pie total"
+end
+
+get "/cart" do
+  @pie = pieNum
+  @cake = cakeNum
+  @cookie = cookieNum
+  erb :cart
 end
---
+++ public/js/index.js
@@ -0,0 +1,21 @@
+
+
+
+// var cookieNum = 0
+function buyCookie() {
+    fetch("http://localhost:4567/test")
+}
+
+function getCake() {
+  fetch("http://localhost:4567/buycake")
+}
+
+function getPie() {
+  fetch("http://localhost:4567/buypie")
+}
+
+// var buyCake = document.getElementById('buyCake');
+// var buyPie = document.getElementById('buyPie');
+//
+// var cakeNum = 0
+// var pieNum = 0
